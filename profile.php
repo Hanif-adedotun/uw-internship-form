7ca4f745a50aa753b89c449e82bf3a788abf24a8
Hanif Adedotun <54331442+Hanif-adedotun@users.noreply.github.com>
2021-04-13 17:24:37 +0100

    Add files via upload

--- profile.php
+++ profile.php
@@ -15,7 +15,7 @@ require 'testdb.php';
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
       <!-- Css file -->
       <link rel="stylesheet" href="css/style.css" type="text/css">
 
@@ -24,5 +24,21 @@ require 'testdb.php';
 <body>
     <h1>Welcome intern <?php echo $username ?></h1>
      <p>Your email is <?php echo $_POST['email'] ?></p>
+     <div class="container-fluid">
+      <div class="row align-items-start">
+        <div class="col">
+        <div class="col-head">Your project timeline</div>
+          
+        </div>
+        <div class="col">
+          <div class="col-head">Personal Information</div>
+          <div>
+          <div class="intern-avatar"><i class="bi bi-person-square"></i></div>
+          </div>
+        </div>
+        <div class="col">
+        <div class="col-head">Completed projects</div>
+        </div>
+   </div>
 </body>
 </html>
